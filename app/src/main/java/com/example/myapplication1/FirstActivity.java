56ba553d3b35d9ab99ea2507ae9cf75dd0ff9b5b
jiaweiding <jiaweiding1998@163.com>
2021-03-24 16:37:02 +0800

    add firstActivity

--- app/src/main/java/com/example/myapplication1/FirstActivity.java
+++ app/src/main/java/com/example/myapplication1/FirstActivity.java
@@ -3,16 +3,12 @@ package com.example.myapplication1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public  void onClick(){
-
+        setContentView(R.layout.activity_first);
     }
 }
